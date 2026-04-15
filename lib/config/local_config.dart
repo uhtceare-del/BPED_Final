4490d3cf4d2d573b8d54612416f840a7cb98110d
uhtceare-del <adarenkerby@gmail.com>
2026-04-15 15:19:02 +0800

    Add local_config for team collaboration

---
+++ lib/config/local_config.dart
@@ -0,0 +1,6 @@
+class LocalConfig {
+  static const String supabaseUrl =
+      'https://itgopuoizfhkfgowamli.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0Z29wdW9pemZoa2Znb3dhbWxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTQxNDIsImV4cCI6MjA5MTA5MDE0Mn0.sHhH7mALNcITmY8k_B9xsft-sbOhP3OiLtc9tiNJZhk';
+}
